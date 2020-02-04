9dd6e29037a20a92c56114ae5f10f33b3b7e8b35
Fadzril <fadzril@jomcode.com>
2020-02-04 19:49:34 +0800

    Update variables and flow

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,23 +7,18 @@ echo "⚡️ Starting deployment action"
 # Here we are using the variables
 # Creating the repository URL in this way will allow us to `git push` without providing a password
 # All thanks to the REPO_TOKEN that will grant us access to the repository
-REF="${GITHUB_REF/refs\/head\/}"
+REF="${GITHUB_REF/refs\/head\//}"
 DEPLOYMENT_REPO="kfit-dev/app-defs"
-DEPLOYMENT_REF="deployment/${APP_NAME}/$REF/${IMAGE_TAG}"
 REMOTE_REPO="https://${REPO_TOKEN}@github.com/$DEPLOYMENT_REPO.git"
-GITHUB_REPOSITORY="kfit-dev/app-defs"
 GITHUB_EMAIL="devci"
 GITHUB_USER="dev+ci@kfit.com"
 
 # We need to clone the repo here.
 # Remember, our Docker container is practically pristine at this point
-git clone $REMOTE_REPO repo
-cd repo
+git clone --depth 1 --branch "master" $REMOTE_REPO repo
+echo "⚡️ Clone $REMOTE_REPO master..."
 
-# Install all of our dependencies inside the container
-# based on the git repository Gemfile
-echo "⚡️ Checkout to $DEPLOYMENT_REF branch..."
-git checkout -b $DEPLOYMENT_REF
+cd repo
 
 # Update image tag using kustomize
 kustomize edit set image ${APP_NAME}=${IMAGE_PATH}
@@ -32,16 +27,15 @@ kustomize edit set image ${APP_NAME}=${IMAGE_PATH}
 # So we can perform a commit
 git config user.name "$GITHUB_USER"
 git config user.email "$GITHUB_EMAIL"
+
 git add .
 
 # That will create a nice commit message
-# like Set fave.staging.web image tag at
-# refs/heads/deployment/favefood/staging to 069367f
-git commit -m "Set ${APP_NAME} image tag at $DEPLOYMENT_REF to ${IMAGE_TAG}"
-echo "⚡️ Checkout branch ready to go. Pushing to Github..."
+git commit -m "Deploy kfit-dev/@$APP_NAME to $DEPLOY_PATH"
+echo "⚡️ Changes for image $IMAGE_PATH ready to go. Pushing to Github..."
 
-# Force push this update to our gh-pages
-git push --force origin $DEPLOYMENT_REF
+# Push this update to our master
+git push origin master
 
 # Now everything is ready.
 # Lets just be a good citizen and so some clean-up after ourselves
