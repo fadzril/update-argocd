da8465a9f613fac864b7d73a2ccacf9b5789d794
Fadzril <fadzril@jomcode.com>
2020-02-05 00:19:39 +0800

    Update entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,18 @@
 # Container image that runs your code
-FROM alpine:3.10
+FROM ubuntu:latest
+
 LABEL author="Fadzril"
-LABEL version="1.0.0"
+LABEL version="1.2.0"
+
+ARG DEBIAN_FRONTEND=noninteractive
 
 # Install dependencies
 RUN apt-get update && \
     apt-get install --no-install-recommends -y \
-        build-essential \
-        ca-certificates \
-        git-all
+    git-all curl
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["sh", "/entrypoint.sh"]
-
-
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,21 +7,28 @@ echo "⚡️ Starting deployment action"
 # Here we are using the variables
 # Creating the repository URL in this way will allow us to `git push` without providing a password
 # All thanks to the REPO_TOKEN that will grant us access to the repository
-REF="${GITHUB_REF/refs\/head\//}"
-DEPLOYMENT_REPO="kfit-dev/app-defs"
-REMOTE_REPO="https://${REPO_TOKEN}@github.com/$DEPLOYMENT_REPO.git"
+REMOTE_REPO="https://$REPO_TOKEN@github.com/kfit-dev/app-defs.git"
 GITHUB_EMAIL="devci"
 GITHUB_USER="dev+ci@kfit.com"
 
 # We need to clone the repo here.
 # Remember, our Docker container is practically pristine at this point
-git clone --depth 1 --branch "master" $REMOTE_REPO repo
 echo "⚡️ Clone $REMOTE_REPO master..."
+git clone $REMOTE_REPO repo
 
 cd repo
 
+echo "⚡️ Checkout branch deploy/$APP_PATH"
+git checkout deploy/$APP_PATH
+
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
+chmod +x ./kustomize
+cp ./kustomize /usr/local/bin/kustomize
+echo "⚡️ Validating kustomize: $(which kustomize)"
+
 # Update image tag using kustomize
-kustomize edit set image ${APP_NAME}=${IMAGE_PATH}
+cd deploy/$APP_PATH
+./kustomize edit set image ${APP_NAME}=${IMAGE_PATH}
 
 # Now we setup default config for committer
 # So we can perform a commit
@@ -31,11 +38,11 @@ git config user.email "$GITHUB_EMAIL"
 git add .
 
 # That will create a nice commit message
-git commit -m "Deploy kfit-dev/@$APP_NAME to $DEPLOY_PATH"
+git commit -m "Deploy kfit-dev/@$APP_NAME to $APP_PATH with ref $IMAGE_TAG"
 echo "⚡️ Changes for image $IMAGE_PATH ready to go. Pushing to Github..."
 
 # Push this update to our master
-git push origin master
+git push origin deploy/$APP_PATH
 
 # Now everything is ready.
 # Lets just be a good citizen and so some clean-up after ourselves
